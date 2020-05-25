f88aaf9cc1fa4af46f346a21d1a50d6b368c7b7b
Sean Beagle <30675442+SeanBeagle@users.noreply.github.com>
2020-05-25 14:55:25 -0600

    Update seanbeagle.framework.js

--- js/seanbeagle.framework.js
+++ js/seanbeagle.framework.js
@@ -1,5 +1,6 @@
+console.log('LOADING: seanbeagle.framework.js');
 $(document).ready( () => {
   $('#header').load('header.html');
   $('#body').load('home.html');
   $('#footer').load('footer.html');
-);
+});
