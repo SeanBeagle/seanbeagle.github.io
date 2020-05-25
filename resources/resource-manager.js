0b4af3a9ec4adfd6b1768da6383d1fa6d851b076
Sean Beagle <30675442+SeanBeagle@users.noreply.github.com>
2020-05-25 09:56:15 -0600

    Update resource-manager.js

--- resources/resource-manager.js
+++ resources/resource-manager.js
@@ -1,12 +1,21 @@
 console.log('Loading resource-manager.js');
+
 function formatBook(book) {
    return '<li>' + book.author + ' (' + book.year + '). <a href="' + book.link + '">' + book.title + '</a>. ' + book.location + ': ' + book.publisher + '</li>';
 }
 
 var books = [];
-$.getJSON('resources.json', data => {
-    data.forEach(book => books.push(book));
-});
+$.getJSON('resources.json', d => d.forEach(book => books.push(book)));
+
+/* BUILD HTML FRAMEWORK */
+// books
+$('<ol/>').attr('id', 'books').appendTo('#resources');
+$('<h5/>').text('Books').appendTo('#books');
+// blogs
+$('<ol/>').attr('id', 'blogs').appendTo('#resources');
+$('<h5/>').text('Blogs').appendTo('#blogs');
+
+// Add books to HTML
 books.sort((a, b) => a.author < b.author);
 books.forEach(book => $(formatBook(book)).appendTo('#books-list'));
 books.forEach(item => console.log(item));
